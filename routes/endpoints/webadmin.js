2ebbbff983ad13565f16542bae7a204bdefaa503
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 13:16:47 +0100

    Update webadmin.js

--- routes/endpoints/webadmin.js
+++ routes/endpoints/webadmin.js
@@ -1,7 +1,11 @@
 const Webadmin = require('../../models/webadmin');
 
 let routes = (app) =>{
-
+app.all('/webadmins', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.post("/webadmins",async(req, res)=>{
 	try{
 		let webadmin = new Webadmin(req.body)

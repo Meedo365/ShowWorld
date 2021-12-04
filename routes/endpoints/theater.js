ba2d1219b20cd6af90d439c7bc5e4158ed43cf9a
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 12:47:30 +0100

    Update theater.js

--- routes/endpoints/theater.js
+++ routes/endpoints/theater.js
@@ -1,7 +1,11 @@
 const Theater = require('../../models/theater');
 
 let routes = (app) => {
-
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 	app.post("/theater", async (req, res) => {
 		try {

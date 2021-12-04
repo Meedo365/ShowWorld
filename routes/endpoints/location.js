c8b3208a544806a657ee8e438438d9dc34245174
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 12:53:11 +0100

    Update location.js

--- routes/endpoints/location.js
+++ routes/endpoints/location.js
@@ -1,7 +1,11 @@
 const Location = require('../../models/location');
 
 let routes = (app) => {
-
+	app.all('/locations', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 	app.post("/location", async (req, res) => {
 		try {

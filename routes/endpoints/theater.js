d0f9f3687f01ad08d67f55a15e68fe0198d17a11
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 12:51:56 +0100

    Update theater.js

--- routes/endpoints/theater.js
+++ routes/endpoints/theater.js
@@ -1,7 +1,7 @@
 const Theater = require('../../models/theater');
 
 let routes = (app) => {
-app.all('/theater', function(req, res, next) {
+app.all('/theaters', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()

611ddf3428a06a7bb81ab4422b0fd14d24f143d9
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 13:37:52 +0100

    Update counter.js

--- routes/endpoints/counter.js
+++ routes/endpoints/counter.js
@@ -1,6 +1,11 @@
 const Counter = require('../../models/counter');
 
 let routes = (app) =>{
+	app.all('/cpunters', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 app.post("/counters",async(req, res)=>{
 	try{

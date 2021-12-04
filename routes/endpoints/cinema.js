bb02546daad7a4275cd4a5a2f0915b862f1ced22
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 13:22:30 +0100

    Update cinema.js

--- routes/endpoints/cinema.js
+++ routes/endpoints/cinema.js
@@ -1,7 +1,11 @@
 const Cinema = require('../../models/cinema');
 
 let routes = (app) => {
-
+app.all('/cinemas', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 	app.post("/cinema", async (req, res) => {
 		try {

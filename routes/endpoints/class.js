aee95cf21d479c3f03312392c0a9ea05efaf2000
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 13:23:07 +0100

    Update class.js

--- routes/endpoints/class.js
+++ routes/endpoints/class.js
@@ -1,7 +1,11 @@
 const TicketClass = require('../../models/class');
 
 let routes = (app) =>{
-
+app.all('/classes', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 app.post("/class",async(req, res)=>{
 	try {

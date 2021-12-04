9ad1589e4f4882849eb83856718d48e962a29c81
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 12:48:43 +0100

    Update upcoming.js

--- routes/endpoints/upcoming.js
+++ routes/endpoints/upcoming.js
@@ -11,6 +11,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single('logo');
 
 let routes = (app) => {
+	app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 	app.post('/upcoming', async (req, res) => {
 		upload(req, res, async (err) => {
 			if (err) {

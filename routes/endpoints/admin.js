0a8a70161da01eb3df7a19ee908567e64fb3593b
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-04 13:12:22 +0100

    Update admin.js

--- routes/endpoints/admin.js
+++ routes/endpoints/admin.js
@@ -1,6 +1,11 @@
 const Admin = require('../../models/admin');
 
 let routes = (app) => {
+	app.all('/admins', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 	app.post('/admins', async (req, res) => {
 		upload(req, res, async (err) => {
 			if (err) {

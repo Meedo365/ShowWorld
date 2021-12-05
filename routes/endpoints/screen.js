e04ec5bdb8dbbd8e5e71e065b79c2bcc63f2de43
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-05 16:24:00 +0100

    Update screen.js

--- routes/endpoints/screen.js
+++ routes/endpoints/screen.js
@@ -1,6 +1,11 @@
 const Screen = require('../../models/screen');
 
 let routes = (app) => {
+	app.all('/screens', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 	app.post("/screen", async (req, res) => {
 		try {

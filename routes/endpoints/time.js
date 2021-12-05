629667991f596a9407c26f22eb1ac244ae832913
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2021-12-05 16:25:46 +0100

    Update time.js

--- routes/endpoints/time.js
+++ routes/endpoints/time.js
@@ -1,7 +1,11 @@
 const Time = require('../../models/time');
 
 let routes = (app) => {
-
+app.all('/times', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 
 	app.post("/time", async (req, res) => {
 		try {
